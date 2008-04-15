c0b6ee8f1bdd61c602cfda27e4edce00441d2e73
nsieger <nsieger@97a6c6ee-731e-0410-87e5-e5612b5efbfe>
2008-04-15 18:22:21 +0000

    Change license to MIT
    git-svn-id: https://svn.codehaus.org/jruby-contrib/trunk/rack@138 97a6c6ee-731e-0410-87e5-e5612b5efbfe

--- spec/rack/application_spec.rb
+++ spec/rack/application_spec.rb
@@ -1,6 +1,6 @@
 #--
 # Copyright 2007-2008 Sun Microsystems, Inc.
-# This source code is available under a CPL 1.0/GPL 2.0/LGPL 2.1 tri-license.
+# This source code is available under the MIT license.
 # See the file LICENSE.txt for details.
 #++
 
