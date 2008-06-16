48214dc7eb811aa5acf685d1713f26a797e58f54
nsieger <nsieger@97a6c6ee-731e-0410-87e5-e5612b5efbfe>
2008-06-16 20:55:12 +0000

    Experimental lazy-loading environment hash to minimize java integration calls
    - TODO: Need to ensure that Rack doesn't expect to use Hash#has_key?
    
    
    git-svn-id: https://svn.codehaus.org/jruby-contrib/trunk/rack@180 97a6c6ee-731e-0410-87e5-e5612b5efbfe

--- src/main/ruby/rack/handler/servlet.rb
+++ src/main/ruby/rack/handler/servlet.rb
@@ -14,60 +14,135 @@ module Rack
       end
 
       def call(servlet_env)
-        env = env_hash
-        add_input_errors_scheme(servlet_env, env)
-        add_servlet_request_attributes(servlet_env, env)
-        add_variables(servlet_env, env)
-        add_headers(servlet_env, env)
-        JRuby::Rack::Response.new(@rack_app.call(env))
+        JRuby::Rack::Response.new(@rack_app.call(create_env(servlet_env)))
       end
 
-      def env_hash
-        { "rack.version" => Rack::VERSION, "rack.multithread" => true,
-          "rack.multiprocess" => false, "rack.run_once" => false }
+      def create_env(servlet_env)
+        LazyEnv.new(servlet_env).to_hash
+      end
+    end
+
+    class LazyEnv
+      def initialize(servlet_env)
+        @env = Hash.new {|h,k| load_env_key(h,k)}
+        @servlet_env = servlet_env
+        load_servlet_request_attributes
       end
 
-      def add_input_errors_scheme(servlet_env, env)
-        env['rack.input'] = servlet_env.to_io
-        env['rack.errors'] = JRuby::Rack::ServletLog.new
-        env['rack.url_scheme'] = servlet_env.getScheme
-        env['java.servlet_request'] = servlet_env
-        env['java.servlet_context'] = $servlet_context
+      def to_hash
+        @env
       end
 
-      def add_servlet_request_attributes(servlet_env, env)
-        servlet_env.getAttributeNames.each do |k|
-          env[k] = servlet_env.getAttribute(k)
+      def load_env_key(env, key)
+        if respond_to?("load__#{key}")
+          send("load__#{key}", env)
+        elsif key =~ /^(rack|java)/
+          load_builtin(env, key)
+        elsif key =~ /^HTTP_/
+          load_headers(env, key)
         end
       end
 
-      def add_variables(servlet_env, env)
-        context_path = servlet_env.getContextPath || ""
-        env["REQUEST_METHOD"] ||= servlet_env.getMethod || "GET"
-        env["SCRIPT_NAME"]    ||= "#{context_path}#{servlet_env.getServletPath}"
-        env["REQUEST_URI"]    ||= servlet_env.getRequestURI || ""
-        path_info = servlet_env.getServletPath || ""
-        path_info += servlet_env.getPathInfo if servlet_env.getPathInfo
-        env["PATH_INFO"]      ||= path_info
-        env["QUERY_STRING"]   ||= servlet_env.getQueryString || ""
-        env["SERVER_NAME"]    ||= servlet_env.getServerName || ""
-        env["REMOTE_HOST"]    ||= servlet_env.getRemoteHost || ""
-        env["REMOTE_ADDR"]    ||= servlet_env.getRemoteAddr || ""
-        env["REMOTE_USER"]    ||= servlet_env.getRemoteUser || ""
-        env["SERVER_PORT"]    ||= servlet_env.getServerPort
-        env["SERVER_PORT"]      = env["SERVER_PORT"].to_s unless String === env["SERVER_PORT"]
+      def load_servlet_request_attributes
+        @servlet_env.getAttributeNames.each do |k|
+          v = @servlet_env.getAttribute(k)
+          case k
+          when "SERVER_PORT", "CONTENT_LENGTH"
+            @env[k] = v.to_s if v.to_i >= 0
+          when "CONTENT_TYPE"
+            @env[k] = v if v
+          else
+            if v
+              @env[k] = v
+            else
+              @env[k] = ""
+            end
+          end
+        end
       end
 
-      def add_headers(servlet_env, env)
-        env["CONTENT_TYPE"] ||= servlet_env.getContentType
-        env.delete("CONTENT_TYPE") unless env["CONTENT_TYPE"]
-        env["CONTENT_LENGTH"] ||= servlet_env.getContentLength
-        env["CONTENT_LENGTH"] = env["CONTENT_LENGTH"].to_s unless String === env["CONTENT_LENGTH"]
-        env.delete("CONTENT_LENGTH") unless env["CONTENT_LENGTH"] && env["CONTENT_LENGTH"].to_i >= 0
-        servlet_env.getHeaderNames.each do |h|
-          next if h =~ /^Content-(Type|Length)$/i
-          env["HTTP_#{h.upcase.gsub(/-/, '_')}"] ||= servlet_env.getHeader(h)
+      def load_headers(env, key)
+        unless @headers_added
+          @headers_added = true
+          @servlet_env.getHeaderNames.each do |h|
+            next if h =~ /^Content-(Type|Length)$/i
+            k = "HTTP_#{h.upcase.gsub(/-/, '_')}"
+            env[k] = @servlet_env.getHeader(h) unless env.has_key?(k)
+          end
         end
+        if env.has_key?(key)
+          env[key]
+        else
+          nil
+        end
+      end
+
+      def load_builtin(env, key)
+        case key
+        when 'rack.version'         then env[key] = Rack::VERSION
+        when 'rack.multithread'     then env[key] = true
+        when 'rack.multiprocess'    then env[key] = false
+        when 'rack.run_once'        then env[key] = false
+        when 'rack.input'           then env[key] = @servlet_env.to_io
+        when 'rack.errors'          then env[key] = JRuby::Rack::ServletLog.new
+        when 'rack.url_scheme'      then env[key] = @servlet_env.getScheme
+        when 'java.servlet_request' then env[key] = @servlet_env
+        when 'java.servlet_context' then env[key] = $servlet_context
+        else
+          nil
+        end
+      end
+
+      def load__CONTENT_TYPE(env)
+        env["CONTENT_TYPE"] = @servlet_env.getContentType
+      end
+
+      def load__CONTENT_LENGTH(env)
+        content_length = @servlet_env.getContentLength
+        env["CONTENT_LENGTH"] = content_length.to_s if content_length >= 0
+      end
+
+      def load__REQUEST_METHOD(env)
+        env["REQUEST_METHOD"] = @servlet_env.getMethod || "GET"
+      end
+
+      def load__SCRIPT_NAME(env)
+        context_path = @servlet_env.getContextPath || ""
+        env["SCRIPT_NAME"] = "#{context_path}#{@servlet_env.getServletPath}"
+      end
+
+      def load__REQUEST_URI(env)
+        env["REQUEST_URI"] = @servlet_env.getRequestURI || ""
+      end
+
+      def load__PATH_INFO(env)
+        path_info = @servlet_env.getServletPath || ""
+        path_info += @servlet_env.getPathInfo if @servlet_env.getPathInfo
+        env["PATH_INFO"] = path_info
+      end
+
+      def load__QUERY_STRING(env)
+        env["QUERY_STRING"] = @servlet_env.getQueryString || ""
+      end
+
+      def load__SERVER_NAME(env)
+        env["SERVER_NAME"] = @servlet_env.getServerName || ""
+      end
+
+      def load__REMOTE_HOST(env)
+        env["REMOTE_HOST"] = @servlet_env.getRemoteHost || ""
+      end
+
+      def load__REMOTE_ADDR(env)
+        env["REMOTE_ADDR"] = @servlet_env.getRemoteAddr || ""
+      end
+
+      def load__REMOTE_USER(env)
+        env["REMOTE_USER"] = @servlet_env.getRemoteUser || ""
+      end
+
+      def load__SERVER_PORT(env)
+        env["SERVER_PORT"] = @servlet_env.getServerPort.to_s
       end
     end
   end
--- src/spec/ruby/rack/handler/servlet_spec.rb
+++ src/spec/ruby/rack/handler/servlet_spec.rb
@@ -15,29 +15,30 @@ describe Rack::Handler::Servlet do
     @servlet_env = mock "servlet request"
   end
 
-  describe "env_hash" do
+  describe "create_env" do
+    before :each do
+      @servlet_env.stub!(:getAttributeNames).and_return []
+    end
+
     it "should create a hash with the Rack variables in it" do
-      hash = @servlet.env_hash
+      hash = @servlet.create_env(@servlet_env)
       hash['rack.version'].should == Rack::VERSION
       hash['rack.multithread'].should == true
       hash['rack.multiprocess'].should == false
       hash['rack.run_once'].should == false
     end
-  end
 
-  describe "add_servlet_request_attributes" do
     it "should add all attributes from the servlet request" do
-      @servlet_env.stub!(:getAttributeNames).and_return ["PATH_INFO", "custom.attribute"]
+      @servlet_env.should_receive(:getAttributeNames).and_return ["PATH_INFO", "custom.attribute"]
       attrs = {"PATH_INFO" => "/path/info", "custom.attribute" => true}
       (class << @servlet_env; self; end).send(:define_method, :getAttribute) {|k| attrs[k]}
-      env = {}
-      @servlet.add_servlet_request_attributes @servlet_env, env
+      env = @servlet.create_env @servlet_env
       env["PATH_INFO"].should == "/path/info"
       env["custom.attribute"].should == true
     end
 
-    it "should be able to override cgi variables" do
-      @servlet_env.stub!(:getAttributeNames).and_return(
+    it "should be able to override cgi variables with request attributes of the same name" do
+      @servlet_env.should_receive(:getAttributeNames).and_return(
           %w(REQUEST_METHOD SCRIPT_NAME PATH_INFO REQUEST_URI QUERY_STRING
              SERVER_NAME SERVER_PORT REMOTE_HOST REMOTE_ADDR REMOTE_USER))
       attrs = { "REQUEST_METHOD" => "POST", "SCRIPT_NAME" => "/override",
@@ -58,9 +59,7 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getRemoteAddr).and_return "127.0.0.1"
       @servlet_env.stub!(:getRemoteUser).and_return "admin"
 
-      env = {}
-      @servlet.add_servlet_request_attributes(@servlet_env, env)
-      @servlet.add_variables(@servlet_env, env)
+      env = @servlet.create_env @servlet_env
 
       env["REQUEST_METHOD"].should == "POST"
       env["SCRIPT_NAME"].should == "/override"
@@ -74,8 +73,8 @@ describe Rack::Handler::Servlet do
       env["REMOTE_USER"].should == "override"
     end
 
-    it "should be able to override headers" do
-      @servlet_env.stub!(:getAttributeNames).and_return(
+    it "should be able to override headers with request attributes named HTTP_*" do
+      @servlet_env.should_receive(:getAttributeNames).and_return(
         %w(HTTP_HOST HTTP_ACCEPT HTTP_ACCEPT_ENCODING CONTENT_TYPE CONTENT_LENGTH))
       attrs = {"HTTP_HOST" => "override", "HTTP_ACCEPT" => "application/*",
         "HTTP_ACCEPT_ENCODING" => "bzip2", "CONTENT_TYPE" => "application/override",
@@ -87,33 +86,38 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getContentLength).and_return(10)
       (class << @servlet_env; self; end).send(:define_method, :getHeader) {|h| enum[h] }
 
-      env = {}
-      @servlet.add_servlet_request_attributes(@servlet_env, env)
-      @servlet.add_headers(@servlet_env, env)
+      env = @servlet.create_env @servlet_env
       env["CONTENT_TYPE"].should == "application/override"
       env["CONTENT_LENGTH"].should == "20"
       env["HTTP_HOST"].should == "override"
       env["HTTP_ACCEPT"].should == "application/*"
       env["HTTP_ACCEPT_ENCODING"].should == "bzip2"
     end
-  end
 
-  describe "add_input_errors_scheme" do
+    it "should not be able to override CONTENT_TYPE or CONTENT_LENGTH to nil" do
+      @servlet_env.should_receive(:getAttributeNames).and_return(%w(CONTENT_TYPE CONTENT_LENGTH))
+      attrs = {"CONTENT_TYPE" => nil, "CONTENT_LENGTH" => -1 }
+      (class << @servlet_env; self; end).send(:define_method, :getAttribute) {|k| attrs[k]}
+      @servlet_env.stub!(:getContentType).and_return "text/html"
+      @servlet_env.stub!(:getContentLength).and_return 10
+
+      env = @servlet.create_env @servlet_env
+      env["CONTENT_TYPE"].should == "text/html"
+      env["CONTENT_LENGTH"].should == "10"
+    end
+
     it "should set the input and error keys" do
       @servlet_env.stub!(:to_io).and_return StringIO.new
       @servlet_env.stub!(:getScheme).and_return "http"
       @servlet_env.stub!(:getContextPath).and_return "/foo"
-      env = {}
-      @servlet.add_input_errors_scheme @servlet_env, env
+      env = @servlet.create_env @servlet_env
       (input = env['rack.input']).should_not be_nil
       [:gets, :read, :each].each {|sym| input.respond_to?(sym).should == true }
       (errors = env['rack.errors']).should_not be_nil
       [:puts, :write, :flush].each {|sym| errors.respond_to?(sym).should == true }
-      env['java.servlet_request'].should_not be_nil
+      env['java.servlet_request'].should == @servlet_env
     end
-  end
 
-  describe "add_variables" do
     it "should add cgi variables" do
       @servlet_env.stub!(:getMethod).and_return "GET"
       @servlet_env.stub!(:getContextPath).and_return "/app"
@@ -126,8 +130,7 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getRemoteHost).and_return "localhost"
       @servlet_env.stub!(:getRemoteAddr).and_return "127.0.0.1"
       @servlet_env.stub!(:getRemoteUser).and_return "admin"
-      env = {}
-      @servlet.add_variables(@servlet_env, env)
+      env = @servlet.create_env @servlet_env
       env["REQUEST_METHOD"].should == "GET"
       env["SCRIPT_NAME"].should == "/app/script_name"
       env["PATH_INFO"].should == "/script_name/path/info"
@@ -145,24 +148,23 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getMethod).and_return nil
       @servlet_env.stub!(:getServletPath).and_return ""
       @servlet_env.stub!(:getPathInfo).and_return nil
-      @servlet_env.stub!(:getRequestURI).and_return nil    
+      @servlet_env.stub!(:getRequestURI).and_return nil
       @servlet_env.stub!(:getQueryString).and_return nil
       @servlet_env.stub!(:getServerName).and_return nil
       @servlet_env.stub!(:getRemoteHost).and_return nil
       @servlet_env.stub!(:getRemoteAddr).and_return nil
       @servlet_env.stub!(:getRemoteUser).and_return nil
       @servlet_env.stub!(:getServerPort).and_return 80
-      env = {}
-      @servlet.add_variables(@servlet_env, env)
-      env.should have_key("REQUEST_METHOD")
-      env.should have_key("SCRIPT_NAME")
-      env.should have_key("PATH_INFO")
-      env.should have_key("REQUEST_URI")
-      env.should have_key("QUERY_STRING")
-      env.should have_key("SERVER_NAME")
-      env.should have_key("REMOTE_HOST")
-      env.should have_key("REMOTE_ADDR")
-      env.should have_key("REMOTE_USER")
+      env = @servlet.create_env @servlet_env
+      env["REQUEST_METHOD"].should == "GET"
+      env["SCRIPT_NAME"].should == ""
+      env["PATH_INFO"].should == ""
+      env["REQUEST_URI"].should == ""
+      env["QUERY_STRING"].should == ""
+      env["SERVER_NAME"].should == ""
+      env["REMOTE_HOST"].should == ""
+      env["REMOTE_ADDR"].should == ""
+      env["REMOTE_USER"].should == ""
     end
 
     it "should calculate path info from the servlet path and the path info" do
@@ -177,13 +179,10 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getRemoteAddr).and_return nil
       @servlet_env.stub!(:getRemoteUser).and_return nil
       @servlet_env.stub!(:getServerPort).and_return 80
-      env = {}
-      @servlet.add_variables(@servlet_env, env)
+      env = @servlet.create_env @servlet_env
       env["PATH_INFO"].should == "/path"
     end
-  end
 
-  describe "add_headers" do
     it "should put content type and content length in the hash without the HTTP_ prefix" do
       enum = {"Content-Type" => "text/plain"}
       @servlet_env.stub!(:getHeaderNames).and_return enum.keys
@@ -192,12 +191,13 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getHeader).and_return {|h| enum[h]}
       (class << @servlet_env; self; end).send(:define_method, :getHeader) {|h| enum[h] }
 
-      env = {}
-      @servlet.add_headers(@servlet_env, env)
+      env = @servlet.create_env @servlet_env
       env["CONTENT_TYPE"].should == "text/html"
       env["CONTENT_LENGTH"].should == "10"
+      env["HTTP_CONTENT_TYPE"].should == nil
       env.should_not have_key?("HTTP_CONTENT_TYPE")
-      env.should_not have_key?("HTTP_CONTENT_LENGTH")    
+      env["HTTP_CONTENT_LENGTH"].should == nil
+      env.should_not have_key?("HTTP_CONTENT_LENGTH")
     end
 
     it "should put the other headers in the hash upcased and underscored and prefixed with HTTP_" do
@@ -208,9 +208,10 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getContentLength).and_return(-1)
       (class << @servlet_env; self; end).send(:define_method, :getHeader) {|h| enum[h] }
 
-      env = {}
-      @servlet.add_headers(@servlet_env, env)
+      env = @servlet.create_env @servlet_env
+      env["CONTENT_TYPE"].should == nil
       env.should_not have_key?("CONTENT_TYPE")
+      env["CONTENT_LENGTH"].should == nil
       env.should_not have_key?("CONTENT_LENGTH")
       env["HTTP_HOST"].should == "localhost"
       env["HTTP_ACCEPT"].should == "text/*"
@@ -225,8 +226,7 @@ describe Rack::Handler::Servlet do
       @servlet_env.stub!(:getContentLength).and_return(-1)
       (class << @servlet_env; self; end).send(:define_method, :getHeader) {|h| enum[h] }
 
-      env = {}
-      @servlet.add_headers(@servlet_env, env)
+      env = @servlet.create_env @servlet_env
       env["HTTP_X_FORWARDED_PROTO"].should == "https"
       env["HTTP_IF_NONE_MATCH"].should == "abcdef"
       env["HTTP_IF_MODIFIED_SINCE"].should == "today"
@@ -242,14 +242,11 @@ describe Rack::Handler::Servlet, "call" do
   end
 
   it "should delegate to the inner application after constructing the env hash" do
-    @servlet.should_receive(:add_input_errors_scheme).ordered
-    @servlet.should_receive(:add_servlet_request_attributes).ordered
-    @servlet.should_receive(:add_variables).ordered
-    @servlet.should_receive(:add_headers).ordered
-    
+    @servlet.should_receive(:create_env).and_return({})
+
     servlet_env = mock("servlet request")
     @app.should_receive(:call)
-    
+
     response = @servlet.call(servlet_env)
     response.should respond_to(:respond)
   end
