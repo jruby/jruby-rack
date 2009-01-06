6ba10933a38fe3025f7c9d5046a5709a765412db
Frederic Jean <frederic.jean@sun.com>
2009-01-05 20:27:30 -0700

    Using the new act_as_publisher and act_as_subscriber methods in the example.

--- examples/rails/app/controllers/queue_controller.rb
+++ examples/rails/app/controllers/queue_controller.rb
@@ -1,12 +1,13 @@
 require 'jruby/rack/queues'
 
 class QueueController < ApplicationController
+  
+  act_as_publisher "rack"
+  act_as_subscriber
+  
   skip_before_filter :verify_authenticity_token
   cattr_accessor :queue_count, :instance_writer => false
   @@queue_count = 0
-
-  include JRuby::Rack::Queues::MessagePublisher::To("rack")
-  extend JRuby::Rack::Queues::MessageSubscriber
 
   subscribes_to "rack" do |msg|
     puts "Received message: #{msg.inspect}"
