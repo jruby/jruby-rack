bef31e797f392193cfd70d8fd04be07a149c2a31
Nick Sieger <nick@nicksieger.com>
2009-04-10 03:03:55 -0500

    Revert "Add Rails::Info to snoop page". That didn't work.
    This reverts commit 36ad7091c275127f40fa1fcaed8b3687cbe61cad.

--- examples/rails/config/environment.rb
+++ examples/rails/config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
+  config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
@@ -38,15 +39,4 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-
-  # Don't use a database configuration or connection
-  def config.database_configuration
-    class << ActiveRecord::Base
-      def establish_connection(spec = nil)
-        nil
-      end
-    end
-    nil
-  end
-
 end
