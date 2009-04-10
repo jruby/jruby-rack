08807e19e12294e64a5a0a7e39e787392469cb5e
Nick Sieger <nick@nicksieger.com>
2009-04-10 02:23:28 -0500

    Need to override logger otherwise Rails will try to write to log/production.log

--- examples/rails/config/environments/production.rb
+++ examples/rails/config/environments/production.rb
@@ -13,7 +13,7 @@ config.action_view.cache_template_loading            = true
 # config.log_level = :debug
 
 # Use a different logger for distributed setups
-# config.logger = SyslogLogger.new
+config.logger = ActiveSupport::BufferedLogger.new(STDOUT)
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
