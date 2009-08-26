5cd62294da27ab0dd8a1139363789200754d3732
Nick Sieger <nick@nicksieger.com>
2009-08-26 15:20:24 -0500

    Add a simple form for testing regular (non-multipart) forms

---
+++ examples/rails/app/controllers/form_controller.rb
@@ -0,0 +1,9 @@
+class FormController < ApplicationController
+  def index
+    if request.post?
+      require 'pp'
+      flash[:message] = "You posted these params:"
+      @params = params.pretty_inspect
+    end
+  end
+end
