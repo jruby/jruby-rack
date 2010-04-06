291228c07d09c9d1b9f7531af7ac07778fa74742
Nick Sieger <nick@nicksieger.com>
2010-04-05 21:25:21 -0500

    Add dummy action_dispatch for specs

---
+++ src/spec/ruby/rails/action_dispatch.rb
@@ -0,0 +1,9 @@
+#--
+# Copyright (c) 2010 Engine Yard, Inc.
+# This source code is available under the MIT license.
+# See the file LICENSE.txt for details.
+#++
+
+# Dummy action_dispatch.rb for specs
+module ActionDispatch
+end
