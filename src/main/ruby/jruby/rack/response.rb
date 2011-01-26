d44fa86aa84aa3e202f952a529738aa89281c9c8
Alejandro Crosa <acrosa@aac-mn.linkedin.biz>
2011-01-25 16:37:27 -0800

    Added chunked responses, and specs

--- src/main/ruby/jruby/rack/response.rb
+++ src/main/ruby/jruby/rack/response.rb
@@ -10,8 +10,6 @@ class JRuby::Rack::Response
 
   def initialize(arr)
     @status, @headers, @body = *arr
-    @chunked = ("chunked" == @headers['Transfer-Encoding'])
-    [@status, @headers, @body]
   end
 
   def getStatus
@@ -20,6 +18,10 @@ class JRuby::Rack::Response
 
   def getHeaders
     @headers
+  end
+
+  def chunked?
+    (@headers && @headers['Transfer-Encoding'] == "chunked")
   end
 
   def getBody
@@ -50,9 +52,7 @@ class JRuby::Rack::Response
       when /^Content-Type$/i
         response.setContentType(v.to_s)
       when /^Content-Length$/i
-        response.setContentLength(v.to_i) unless @chunked
-      when /^Transfer-Encoding$/i
-        # ignore setting Transfer-Encoding, since Tomcat will add the header twice, Jetty on the other hand works fine either way.
+        response.setContentLength(v.to_i) unless chunked?
       else
         if v.respond_to?(:each_line)
           v.each_line {|val| response.addHeader(k.to_s, val.chomp("\n")) }
@@ -77,7 +77,7 @@ class JRuby::Rack::Response
     begin
       @body.each do |el|
         stream.write(el.to_java_bytes)
-        stream.flush if @chunked
+        stream.flush if chunked?
       end
     rescue LocalJumpError => e
       # HACK: deal with objects that don't comply with Rack specification
