cb166fbbd3e47035c741d5936a4543395067a9df
Nick Sieger <nick@nicksieger.com>
2011-02-09 23:18:25 -0600

    Do away with LoadPathDebugging in favor of new Capture modules

---
+++ src/spec/ruby/rack/capture_spec.rb
@@ -0,0 +1,24 @@
+#--
+# Copyright (c) 2010-2011 Engine Yard, Inc.
+# Copyright (c) 2007-2009 Sun Microsystems, Inc.
+# This source code is available under the MIT license.
+# See the file LICENSE.txt for details.
+#++
+
+require 'spec_helper'
+require 'jruby/rack'
+
+describe JRuby::Rack::Capture do
+  before :each do
+    $servlet_context = @servlet_context
+    $servlet_context.stub!(:init_parameter_names).and_return []
+  end
+
+  it "should capture environment information" do
+    @servlet_context.should_receive(:log)
+    StandardError.new.tap do |e|
+      e.capture
+      e.store
+    end
+  end
+end
