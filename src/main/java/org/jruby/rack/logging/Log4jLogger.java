0edcb62621fdf0b721673ac50b9cacc3c4397339
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2011-03-09 23:27:04 -0800

    JRUBY-5573 - Add support for log4j in jruby-rack
    http://jira.codehaus.org/browse/JRUBY-5573

---
+++ src/main/java/org/jruby/rack/logging/Log4jLogger.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2011 ThoughtWorks, Inc.
+ * This source code is available under the MIT license.
+ * See the file LICENSE.txt for details.
+ */
+
+package org.jruby.rack.logging;
+
+import org.jruby.rack.RackLogger;
+import org.apache.log4j.Logger;
+
+public class Log4jLogger implements RackLogger {
+    private Logger logger;
+
+    public Log4jLogger(String loggerName) {
+        setLoggerName(loggerName);
+    }
+
+    public void setLoggerName(String loggerName) {
+        logger = Logger.getLogger(loggerName);
+    }
+
+    @Override
+    public void log(String message) {
+        logger.info(message);
+    }
+
+    @Override
+    public void log(String message, Throwable ex) {
+        logger.error(message, ex);
+    }
+}
