756300d3a0addcf66f545a474a2fa1fc6897b4d3
Nick Sieger <nick@nicksieger.com>
2012-02-01 15:34:39 -0600

    Whitespace

--- src/spec/ruby/jruby/rack/booter_spec.rb
+++ src/spec/ruby/jruby/rack/booter_spec.rb
@@ -98,7 +98,7 @@ describe JRuby::Rack::Booter do
     create_booter.boot!
     $loaded_init_rb.should == true
   end
-  
+
   it "should adjust load path when runtime.jruby_home == /tmp" do
     tmpdir = java.lang.System.getProperty('java.io.tmpdir')
     jruby_home = JRuby.runtime.instance_config.getJRubyHome
@@ -122,9 +122,9 @@ describe JRuby::Rack::Booter do
       $LOAD_PATH << "."
       # "stub" runtime.jruby_home :
       JRuby.runtime.instance_config.setJRubyHome(tmpdir)
-      
+
       create_booter.boot!
-      
+
       expected = []
       if JRuby.runtime.is1_9
         expected << "META-INF/jruby.home/lib/ruby/site_ruby/1.9"
@@ -147,6 +147,6 @@ describe JRuby::Rack::Booter do
       JRuby.runtime.instance_config.setJRubyHome(jruby_home)
     end
   end
-  
+
 end
 
