ef911e1af5f56357b349fd3146456f90406c07d9
kares <self@kares.org>
2012-12-26 09:42:08 +0100

    setup a rails 23 stub for specs

---
+++ src/spec/stub/rails23/WEB-INF/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'da316daecf9af0892ae14b52f48b8f3ab393e90310ca136fc3e5171e1fec4192c1dd99183033f5e3d6af1ccf6c3040ac213f26e6f45c2e0dbbad85f2d64a927e';
