778c17fd825aa4350ade8c9c59d0c7a283f89144
kares <self@kares.org>
2014-08-20 21:14:49 +0200

    cleanup ServletRackEnvironment impl - fail fast in costructor + no implements ToIO

--- src/main/java/org/jruby/rack/servlet/ServletRackEnvironment.java
+++ src/main/java/org/jruby/rack/servlet/ServletRackEnvironment.java
@@ -1,10 +1,10 @@
 /*
+ * Copyright (c) 2013-2014 Karol Bucek LTD.
  * Copyright (c) 2010-2012 Engine Yard, Inc.
  * Copyright (c) 2007-2009 Sun Microsystems, Inc.
  * This source code is available under the MIT license.
  * See the file LICENSE.txt for details.
  */
-
 package org.jruby.rack.servlet;
 
 import java.io.IOException;
@@ -20,44 +20,47 @@ import org.jruby.rack.ext.Input;
 
 /**
  * Rack environment (default) implementation based on {@link HttpServletRequest}..
- * 
+ *
  * @see RackEnvironment
  * @see HttpServletRequest
  * @see HttpServletRequestWrapper
- * 
+ *
  * @author nicksieger
  */
 @SuppressWarnings("unchecked")
 public class ServletRackEnvironment extends HttpServletRequestWrapper
-    implements RackEnvironment, RackEnvironment.ToIO {
-    
+    implements RackEnvironment {
+
     private String scriptName;
     private String requestURI;
     private String requestURIWithoutQuery;
     private String pathInfo;
-    
+
     private final RackContext context;
     private final HttpServletResponse response;
-    
+
     /**
      * Creates an environment instance for the given request, response and context.
      * @param request
      * @param response
-     * @param context 
+     * @param context
      */
     public ServletRackEnvironment(HttpServletRequest request, HttpServletResponse response, RackContext context) {
         super(request);
+        if ( response == null ) throw new IllegalArgumentException("null response");
         this.response = response;
+        if ( context == null ) throw new IllegalArgumentException("null context");
         this.context = context;
     }
 
     /**
-     * @see RackEnvironment#getContext() 
+     * @see RackEnvironment#getContext()
      */
+    @Override
     public RackContext getContext() {
         return context;
     }
-    
+
     /**
      * The underlying HttpServletResponse
      * @return the response
@@ -65,26 +68,26 @@ public class ServletRackEnvironment extends HttpServletRequestWrapper
     public HttpServletResponse getResponse() {
     	return response;
     }
-    
+
     /**
      * @see RackEnvironment#getInput()
      */
+    @Override
     public ServletInputStream getInput() throws IOException {
         return getInputStream();
     }
-    
+
     /**
      * Define the script name as the context path + the servlet path.
      * @return script name
      * @see RackEnvironment#getScriptName()
      */
+    @Override
     public String getScriptName() {
-        if (scriptName != null) {
-            return scriptName;
-        }
+        if ( scriptName != null ) return scriptName;
 
         String contextPath = getContextPath();
-        if (contextPath == null) contextPath = "";
+        if ( contextPath == null ) contextPath = "";
         return scriptName = contextPath.equals("/") ? "" : contextPath;
     }
 
@@ -94,23 +97,24 @@ public class ServletRackEnvironment extends HttpServletRequestWrapper
      * @return full path info
      * @see RackEnvironment#getPathInfo()
      */
-    @Override 
+    @Override
     public String getPathInfo() {
-        if (pathInfo != null) {
-            return pathInfo;
-        }
-        
-        StringBuilder buffer = new StringBuilder();
-        if (getRequestURIWithoutQuery().length() > 0) {
-            if (getScriptName().length() > 0 && getRequestURIWithoutQuery().indexOf(getScriptName()) == 0) {
-                buffer.append(getRequestURIWithoutQuery().substring(getScriptName().length()));
+        if ( pathInfo != null ) return pathInfo;
+
+        final StringBuilder buffer = new StringBuilder(32);
+        final String onlyURI = getRequestURIWithoutQuery();
+        if ( onlyURI.length() > 0 ) {
+            final String script = getScriptName();
+            if ( script != null && script.length() > 0
+                && onlyURI.indexOf(script) == 0 ) {
+                buffer.append( onlyURI.substring(script.length()) );
             } else {
-                buffer.append(getRequestURIWithoutQuery());
+                buffer.append( onlyURI );
             }
         } else {
-            buffer.append(getServletPath());
-            if (super.getPathInfo() != null) {
-                buffer.append(super.getPathInfo());
+            buffer.append( getServletPath() );
+            if ( super.getPathInfo() != null ) {
+                buffer.append( super.getPathInfo() );
             }
         }
         return pathInfo = buffer.toString();
@@ -121,16 +125,14 @@ public class ServletRackEnvironment extends HttpServletRequestWrapper
      * @return URI
      * @see RackEnvironment#getRequestURI()
      */
-    @Override 
+    @Override
     public String getRequestURI() {
-        if (requestURI != null) {
-            return requestURI;
-        }
+        if ( requestURI != null ) return requestURI;
 
-        StringBuilder buffer = new StringBuilder();
-        buffer.append(getRequestURIWithoutQuery());
-        if (super.getQueryString() != null) {
-            buffer.append("?").append(super.getQueryString());
+        final StringBuilder buffer = new StringBuilder(32);
+        buffer.append( getRequestURIWithoutQuery() );
+        if ( super.getQueryString() != null ) {
+            buffer.append('?').append( super.getQueryString() );
         }
         return requestURI = buffer.toString();
     }
@@ -141,24 +143,16 @@ public class ServletRackEnvironment extends HttpServletRequestWrapper
      * @return the request URI
      */
     public String getRequestURIWithoutQuery() {
-        if (requestURIWithoutQuery != null) {
-            return requestURIWithoutQuery;
-        }
-        requestURIWithoutQuery = super.getRequestURI();
-        if (requestURIWithoutQuery == null) {
-            requestURIWithoutQuery = "";
-        }
-        return requestURIWithoutQuery;
+        if ( requestURIWithoutQuery != null ) return requestURIWithoutQuery;
+
+        final String defaultURI = super.getRequestURI();
+        return requestURIWithoutQuery = defaultURI == null ? "" : defaultURI;
     }
 
     private Input io;
-    
-    public Input toIO() {
-        return io;
-    }
-    
-    public void setIO(Input io) {
-        this.io = io;
-    }
-    
+
+    @Deprecated public Input toIO() { return io; }
+
+    @Deprecated public void setIO(Input io) { this.io = io; }
+
 }
