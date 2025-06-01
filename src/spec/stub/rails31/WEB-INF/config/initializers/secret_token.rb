210cbbc6249e6e6510adbbe8e680c6476a6d3933
kares <self@kares.org>
2025-06-01 20:21:14 +0200

    [test] remove the unused Rails stub applications

--- src/spec/stub/rails31/WEB-INF/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Rails31::Application.config.secret_token = 'cbb82869240754463f7772c5095a3e7865939b282f2623c308a0baa223f993772d9c45c1292bd14561e3e88a776fb9be925564756ca409edf8883b7e9cf55b27'
