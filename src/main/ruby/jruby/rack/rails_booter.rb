c5838ca97c81728d3ebf5fb70c09502bbe2e0d62
kares <self@kares.org>
2025-07-17 15:28:12 +0200

    simplify RailsBooter no need to account for old Rails

--- src/main/ruby/jruby/rack/rails_booter.rb
+++ src/main/ruby/jruby/rack/rails_booter.rb
@@ -26,12 +26,22 @@ module JRuby::Rack
       super
       ENV['RAILS_ROOT'] = app_path
       ENV['RAILS_ENV'] = rails_env
-
-      require 'jruby/rack/rails/environment'
-      extend RailsEnvironment
-
-      set_public_root
       self
+    end
+
+    # @return [Rails::Application] the (loaded) application instance
+    def to_app
+      # backward "compatibility" calling #to_app without a #load_environment
+      load_environment
+      ::Rails.application
+    end
+
+    # Loads the Rails environment (*config/environment.rb*).
+    def load_environment
+      require expand_path('config/boot.rb')
+      require 'jruby/rack/rails/railtie'
+      require expand_path('config/environment.rb')
+      require 'jruby/rack/rails/extensions'
     end
 
     protected
@@ -40,12 +50,6 @@ module JRuby::Rack
       if rails_relative_url_root = relative_url_root('rails.relative_url_append')
         ENV['RAILS_RELATIVE_URL_ROOT'] = rails_relative_url_root
       end
-    end
-
-    # @see JRuby::Rack::RailsBooter::Rails2Environment#set_public_root
-    # @see JRuby::Rack::RailsBooter::Rails3Environment#set_public_root
-    def set_public_root
-      # no-op by default - leave as it is
     end
 
     # @deprecated no longer used, replaced with {#run_boot_hooks}
