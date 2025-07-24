61c1b3726a604bdc6404b18943997f23a6c76e39
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-07-24 14:21:17 +0800

    [chore] Remove old Rails 2.x example
    Will use the Rails 3 example as a base for any rejuvenated later rails examples as it's closer anyway.

--- examples/rails/app/controllers/simple_form_controller.rb
+++
@@ -1,9 +0,0 @@
-class SimpleFormController < ApplicationController
-  def index
-    if request.post?
-      require 'pp'
-      flash[:message] = "You posted these params:"
-      @params = params.pretty_inspect
-    end
-  end
-end
--- examples/rails/app/helpers/cache_headers_helper.rb
+++
@@ -1,2 +0,0 @@
-module CacheHeadersHelper
-end
