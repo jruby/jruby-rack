ec88427949b73445f1c1d5a973022436b086792e
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-08-18 18:09:14 +0800

    [feat] Implement compatibility with Rack 3.x specs

--- src/main/ruby/jruby/rack/chunked.rb
+++ src/main/ruby/jruby/rack/chunked.rb
@@ -3,21 +3,23 @@
 # See the file LICENSE.txt for details.
 #++
 
-require 'rack/chunked' # exists since Rack 1.1
+if Rack.release < '3'
+  require 'rack/chunked' # exists since Rack 1.1, removed in Rack 3.0
 
-# Disables the Rack response body chunking performed by `Rack::Chunked::Body`.
-# It is "necessary" since Rails does instantiate the body directly instead of
-# using `Rack::Chunked` as a middleware.
-# 
-# @note This monkey-patch is not required to support chunking with servlets and 
-# won't be applied unless **jruby.rack.response.dechunk** is 'patch' (default).
-# Set **jruby.rack.response.dechunk** to 'true' to simply "dechunk" the body and
-# keep the `Rack::Chunked::Body` class as is (or 'false' to do no de-chunking at
-# all).
-Rack::Chunked::Body.class_eval do
+  # Disables the Rack response body chunking performed by `Rack::Chunked::Body`.
+  # It is "necessary" since Rails does instantiate the body directly instead of
+  # using `Rack::Chunked` as a middleware.
+  #
+  # @note This monkey-patch is not required to support chunking with servlets and
+  # won't be applied unless **jruby.rack.response.dechunk** is 'patch' (default).
+  # Set **jruby.rack.response.dechunk** to 'true' to simply "dechunk" the body and
+  # keep the `Rack::Chunked::Body` class as is (or 'false' to do no de-chunking at
+  # all).
+  Rack::Chunked::Body.class_eval do
 
-  def each(&block)
-    @body.each(&block) # no-chunking on servlets
+    def each(&block)
+      @body.each(&block) # no-chunking on servlets
+    end
+
   end
-  
 end
