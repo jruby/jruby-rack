24d727588b4ba5d0a5fcaa1d675f0415f0d9e661
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-08-19 13:33:32 +0800

    [chore] Clarify reason for Servlet API deprecations

--- src/main/java/org/jruby/rack/servlet/ResponseCapture.java
+++ src/main/java/org/jruby/rack/servlet/ResponseCapture.java
@@ -81,9 +81,9 @@ public class ResponseCapture extends HttpServletResponseWrapper {
         }
     }
 
-    @SuppressWarnings("deprecation")
     @Override
-    @Deprecated
+    @Deprecated // Deprecated in API parent
+    @SuppressWarnings("deprecation")
     public void setStatus(int status, String message) {
         if ( handleStatus(status, false) ) {
             super.setStatus(status, message);
