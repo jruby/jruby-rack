f5c74c298b90634c6655baa7d76f5757d23c7c0c
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-04-17 20:57:13 +0800

    [test] add test for exception capture content

--- src/spec/ruby/rack/capture_spec.rb
+++ src/spec/ruby/rack/capture_spec.rb
@@ -13,15 +13,26 @@ describe JRuby::Rack::Capture do
   before :each do
     JRuby::Rack.context = nil
     $servlet_context = @servlet_context
-    allow(@servlet_context).to receive(:init_parameter_names).and_return []
+    allow(@servlet_context).to receive(:config).and_return Java::OrgJrubyRackEmbed::Config.new
   end
 
   it "captures environment information" do
     expect(@servlet_context).to receive(:log)
-    error = StandardError.new
+    error = StandardError.new "simulated rack start-up failed"
     error.capture
     error.store
     expect(error.output).to be_a StringIO
+    expect(error.output.string).to include "An exception happened during JRuby-Rack startup"
+    expect(error.output.string).to include "simulated rack start-up failed"
+    expect(error.output.string).to include "--- System"
+    expect(error.output.string).to include "jruby #{JRUBY_VERSION}"
+    expect(error.output.string).to include "--- Context Init Parameters:"
+    expect(error.output.string).to include "--- RubyGems"
+    expect(error.output.string).to include "Gem.path:"
+    expect(error.output.string).to include "--- Bundler"
+    expect(error.output.string).to include "Gemfile:"
+    expect(error.output.string).to include "--- JRuby-Rack Config"
+    expect(error.output.string).to include "logger_class_name"
   end
 
   it "captures exception backtrace" do
@@ -33,5 +44,6 @@ describe JRuby::Rack::Capture do
       expect(e.output.string).to match /ZeroDivisionError/
     end
   end
+
 
 end
